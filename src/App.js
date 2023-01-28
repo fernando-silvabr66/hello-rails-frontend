f34237e400fbc19fc262d1db37ccc8e5381ce113
Fernando Silva <fernando.silvabr66@gmail.com>
2023-01-28 17:31:18 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStore';
 
-const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        {' '}
-        Edit
-        <code>src/App.js</code>
-        {' '}
-        and save to reload.
-      </p>
-      <a
-        className="App-link"
-        href="https://reactjs.org"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Learn React
-      </a>
-    </header>
-  </div>
-);
+import './App.css';
+import Greeting from './components/Greeting';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<Greeting />} />
+        </Routes>
+      </Provider>
+    </BrowserRouter>
+  );
+}
 
 export default App;
